82448c97ede7f9c9d466f9e9d5b63757058c1936
Evans Musomi <evans@habahaba.co>
2016-06-23 15:38:10 +0300

    Adds user first specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { FactoryGirl.build(:user)}
+
+  it "has a valid factory" do
+    expect(user).to be_valid
+  end
+  
+  #Responds to methods
+  context "responds to its methods" do
+    it { expect(user).to respond_to(:email) }
+    it { expect(user).to respond_to (:password) }
+    it { expect(user).to respond_to (:password_confirmation) }
+  end
 end
