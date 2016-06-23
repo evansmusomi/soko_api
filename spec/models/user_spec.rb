ba232a7c0e877d0b30c9b23db034d422bc9ee543
Evans Musomi <evans@habahaba.co>
2016-06-23 15:42:22 +0300

    Adds shoulda matchers for spec refactors

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,18 @@ RSpec.describe User, type: :model do
   it "has a valid factory" do
     expect(user).to be_valid
   end
-  
+
   #Responds to methods
   context "responds to its methods" do
     it { expect(user).to respond_to(:email) }
     it { expect(user).to respond_to (:password) }
     it { expect(user).to respond_to (:password_confirmation) }
   end
+
+  #Validations
+  it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:email) }
+  it { should validate_confirmation_of(:password) }
+  it { should allow_value('example@domain.com').for(:email) }
+  
 end
