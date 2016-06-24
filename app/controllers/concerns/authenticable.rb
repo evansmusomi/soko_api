dfcf1589747dc2a73b22b06dcdf7f5695c553711
Evans Musomi <evans.musomi@gmail.com>
2016-06-24 11:31:55 +0300

    Adds authentication with token method to handle access to actions

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -4,4 +4,10 @@ module Authenticable
   def current_user
     @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
   end
+
+  # Devise method ovveride - for authorization
+  def authenticate_with_token!
+    renders json: {errors: "Not authenticated"},
+    status: :unauthorized unless current_user.present?
+  end
 end
