a629bed92bdb952d68cb0f5a8e2460d0e62cb0ab
Evans Musomi <evans.musomi@gmail.com>
2016-06-24 10:06:01 +0300

    Adds user authentication token

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  # Hooks
+  before_create :generate_authentication_token!
+
+  # Validations
+  validates :email, uniqueness: {case_sensitive: false}
+  validates :auth_token, uniqueness: {case_sensitive: false}
+
+  # Generate unique authentication token
+  def generate_authentication_token!
+    begin
+      self.auth_token = Devise.friendly_token
+    end while self.class.exists?(auth_token: auth_token)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,14 +8,30 @@ RSpec.describe User, type: :model do
   end
 
   #Responds to methods
-  it { expect(user).to respond_to(:email) }
-  it { expect(user).to respond_to (:password) }
-  it { expect(user).to respond_to (:password_confirmation) }
+  it { should respond_to(:email) }
+  it { should respond_to (:password) }
+  it { should respond_to (:password_confirmation) }
+  it { should respond_to (:auth_token) }
 
   #Validations
   it { should validate_presence_of(:email) }
   it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
+  it { should validate_uniqueness_of(:auth_token).case_insensitive }
 
+  #Methods
+  describe "#generate_authentication_token!" do
+    it "generates a unique token" do
+      allow(Devise).to receive(:friendly_token).and_return("auniquetoken321")
+      user.generate_authentication_token!
+      expect(user.auth_token).to eql "auniquetoken321"
+    end
+
+    it "generates another token when one already has been taken" do
+      existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken321")
+      user.generate_authentication_token!
+      expect(user.auth_token).not_to eql existing_user.auth_token
+    end
+  end
 end
