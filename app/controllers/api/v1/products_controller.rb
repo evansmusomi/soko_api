775e2496eabe1d9803b9e9f9f9d4a030a768f532
Evans Musomi <evans.musomi@gmail.com>
2016-06-30 15:09:43 +0300

    Adds search class method to filter products

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -4,8 +4,7 @@ class Api::V1::ProductsController < ApplicationController
 
   # Shows product catalog
   def index
-    products = params[:product_ids].present? ? Product.find(params[:product_ids]) : Product.all
-    respond_with products
+    respond_with Product.search(params)
   end
 
   # Gets a specified product's details
