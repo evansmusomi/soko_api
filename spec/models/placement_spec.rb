d8d1e24397b8786b0eea5de37aec3f891345da63
Evans Musomi <evans.musomi@gmail.com>
2016-07-04 11:46:26 +0300

    Decrements the product quantity by placement quantity

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -12,4 +12,12 @@ RSpec.describe Placement, type: :model do
   # Validations
   it { should belong_to :order }
   it { should belong_to :product }
+
+  # Methods
+  describe "#decrement_product_quantity!" do
+    it "decreases the product quantity by the placement quantity" do
+      product = placement.product
+      expect{placement.decrement_product_quantity!}.to change{product.quantity}.by{-placement.quantity}
+    end
+  end
 end
